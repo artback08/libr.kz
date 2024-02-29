b14a78356bbd32cc2b466ea3a48d47784a52749e
artbaci08 <artback08@gmail.com>
2024-02-29 21:44:42 +0600

    fix flex

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -173,33 +173,29 @@
     </svg>
 
 <main>
-  <header class="p-3 text-bg-dark">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+  <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
+      <div class="col-md-3 mb-2 mb-md-0">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <!-- <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg> -->
-          <img class="logo" src="<?php echo URL; ?>/img/logo2.png" alt="libr.kz">
-          libr.kz
-        </a>
+            <!-- <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg> -->
+            <img class="logo" src="<?php echo URL; ?>/img/logo2.png" alt="libr.kz">
+            libr.kz
+          </a>
+      </div>
 
-        <ul class="pl-5 nav col-12  col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="<?php echo URL ?>/writers" class="nav-link px-4 text-white">Авторы</a></li>
-          <li><a href="<?php echo URL ?>/libraries" class="nav-link px-4 text-white">Библиотеки</a></li>
-          <li><a href="<?php echo URL ?>/museums" class="nav-link px-4 text-white">Памятные места</a></li>
-        </ul>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="<?php echo URL ?>/writers" class="nav-link px- text-secondary">Авторы</a></li>
+          <li><a href="<?php echo URL ?>/libraries" class="nav-link px- text-primary">Библиотеки</a></li>
+          <li><a href="<?php echo URL ?>/museums" class="nav-link px- text-primary">Памятные места</a></li>
+      </ul>
 
-        <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Поиск Писателей..." aria-label="Поиск Писателей">
-        </form> -->
-
-        <div class="text-end">
-          <?php if(User::loggedIn()){ ?>
+      <div class="col-md-3 text-end">
+      <?php if(User::loggedIn()){ ?>
             <a href="<?php echo URL; ?>/logout"><button type="button" class="btn btn-outline-light me-2">Выход</button></a>
           <?php }else{ ?>
-            <a href="<?php echo URL; ?>/login"><button type="button" class="btn btn-outline-light me-2">Войти</button></a> 
+            <a href="<?php echo URL; ?>/login"><button type="button" class="btn btn-outline-light text-primary me-2">Войти</button></a> 
           <?php } ?>
-        </div>
-
       </div>
-    </div>
-  </header>
+    </header>
+  </div>
